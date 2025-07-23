ca6f6096b2f2dbe5ac6109dd09920a949a36efb7
TJ-54321 <masudur.turjo@gmail.com>
2025-07-23 16:39:07 +0600

    V_1.01

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def login():
 def team():
     return render_template('team.html')
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
